e33ad75985fde4181cd7c500dc02be1b7a97b172
Matt Glover <matthew.glover@peeq-digital.com>
2014-01-14 12:00:19 +0000

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularFireBaseAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
